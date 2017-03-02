2e115b71fec42c761f955a1c97d047c789e704ab
Rekkuujin <anthonyha1996@gmail.com>
2017-03-01 21:12:01 -0800

    add speed power-up scripts
    new tags: PlatformGenerator, PowerUp
    new sprite: Speed power-up
    SpeedUp prefab needs to be added to scene to instantiate

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+    //Abstract class for powerups
+
+    //When another object intersects with the powerUp's collider, call the Power-Up function
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject otherObject = collider.gameObject;
+
+        if (collider.gameObject.CompareTag("Player 1") || collider.gameObject.CompareTag("Player 2"))
+        {
+            ActivatePowerUp(collider.gameObject);
+        }
+        else if (collider.gameObject.name == "deathTrigger")
+        {
+            destroyPowerUp();
+        }
+            
+    }
+
+    //Override this virtual function with an added effect in a derived class
+    public virtual void ActivatePowerUp(GameObject player) { }
+
+    //destroys the power-up
+    public void destroyPowerUp()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/PowerUpSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpawner : MonoBehaviour {
+
+    GameObject target;
+    float spawnTime = 7f; //seconds between spawning a new power-up
+    int IndexNum;
+    GameObject[] generators;
+
+    int powerUpCount = 1; //change this according to how many power up types are in the game
+	
+    // Use this for initialization
+	void Start () {
+        //Get the list of platform generators
+        generators = GameObject.FindGameObjectsWithTag("PlatformGenerator");
+        Debug.Log(generators);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        spawnTime -= Time.deltaTime;
+        if (spawnTime <= 0)
+        {
+            spawnTime = 7f;
+            IndexNum = Random.Range(0, 2);
+            target = generators[IndexNum];
+            GameObject clone = Instantiate(    GameObject.FindGameObjectsWithTag("PowerUp")[Random.Range(0, powerUpCount - 1)]);
+            clone.transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 1, target.transform.position.z);
+            
+            //Make an instance of a powerup slightly above a platform generator, which would end up above the platform it makes
+               
+        }
+
+
+
+	}
+
+}
