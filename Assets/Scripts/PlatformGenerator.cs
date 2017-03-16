1e33435ea197b636e90b0724fa479f04313b9e14
Henri Yang <henriyang2@gmail.com>
2017-03-15 19:22:05 -0700

    platform generator rework

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -8,97 +8,70 @@ public class PlatformGenerator : MonoBehaviour
     public Transform platformGenerationPoint;
 
     //The min and max amount of time for Random.Range it takes to spawn a platform
-    //public float spawnMin = 2.75f;
-    //public float spawnMax = 3.5f;
-    public float spawnTime = 9f;
+    public float spawnMin = 4f;
+    public float spawnMax = 5f;
+    //public float spawnTime = 5f;
 
     //Just rough platform sizes for playtesting purposes, can tune this if needed
     public float[] platformSizes = new float[] {2f, 2.5f, 3.5f};
 
-    //List to hold all platform permutations
-    public GameObject[] platformPermutationsList;
-
-    //Keep track of the last platform permutation spawned to prevent back to back spawns of same permutation
-    private int lastPlatformPermutationSpawned = -1;
+    private int PLATFORMS_TO_SPAWN = 3;
 
 	void Start () 
     {
         //Kick everything off by spawning two platforms immediately so platform generator doesn't fall behind
+        //SpawnFirstTwoPlatforms();
         SpawnFirstTwoPlatforms();
+        //Spawn();
 	}
        
     void Spawn ()
     {
-        //Commented out object pooling code for now since we're using platform permutations
-        //so I'm not sure how we want to do it with object pooling, can discuss this later
-        /*
-        //Get a platform from the platform pool
-        GameObject obj = ObjectPooler.current.GetPooledObject();
 
-        //Setting up platform location and size
-        //Random.Range(-0.5f, 0.5f) offset to make platforms not spawn right on top of each other (hopefully)
-        //Change size based on the three platform sizes initiated, can tune this if needed
-        obj.transform.position = new Vector3(transform.position.x + Random.Range(-0.5f, 0.5f), transform.position.y, transform.position.z);
-        obj.transform.rotation = transform.rotation;
-        obj.transform.localScale = new Vector3(platformSizes[Random.Range(0, platformSizes.Length)], obj.transform.localScale.y, obj.transform.localScale.z);
-
-        //Enable the object
-        obj.SetActive(true);
-        */
-
-        int platformPermutationToSpawn = -1;
-        bool differentPermutation = false;
-
-        //If a different permutation is not obtained, keep trying to get a different one
-        while (!differentPermutation)
+        for (int i = 0; i < PLATFORMS_TO_SPAWN; i++)
         {
-            platformPermutationToSpawn = Random.Range(0, platformPermutationsList.Length);
+            //Get a platform from the platform pool
+            GameObject obj = ObjectPooler.current.GetPooledObject();
 
-            //If the permutation we got was not the same as the last permutation spawned, break out of while
-            if (platformPermutationToSpawn != lastPlatformPermutationSpawned)
-            {
-                lastPlatformPermutationSpawned = platformPermutationToSpawn;
-                differentPermutation = true;
-            }
+            //Setting up platform location and size
+            //Random.Range(-0.5f, 0.5f) offset to make platforms not spawn right on top of each other (hopefully)
+            //Change size based on the three platform sizes initiated, can tune this if needed
+            obj.transform.position = new Vector3(transform.position.x + Random.Range(-5.3f, 5.3f), transform.position.y + Random.Range(-2f, 2f), transform.position.z);
+            obj.transform.rotation = transform.rotation;
+            obj.transform.localScale = new Vector3(platformSizes[Random.Range(0, platformSizes.Length)], obj.transform.localScale.y, obj.transform.localScale.z);
+
+            //Enable the object
+            obj.SetActive(true);
         }
 
-        GameObject platformPermutationObj = (GameObject)Instantiate(platformPermutationsList[platformPermutationToSpawn]);
-        platformPermutationObj.transform.position = new Vector3(transform.position.x, transform.position.y + 6f, transform.position.z);
-
-        platformPermutationObj.SetActive(true);
-            
-
         //Kick off another spawn after spawnTime
-        Invoke("Spawn", spawnTime);
+        Invoke("Spawn", Random.Range(spawnMin, spawnMax));
     }
-
+        
     void SpawnFirstTwoPlatforms ()
     {
-        //Spawn the first two platforms and then invoke the normal spawn method
+        //Spawn the first two set of platforms and then invoke the normal spawn method
         for (int i = 0; i < 2; i++)
         {
-            int platformPermutationToSpawn = -1;
-            bool differentPermutation = false;
 
-            while (!differentPermutation)
+            for (int j = 0; j < PLATFORMS_TO_SPAWN; j++)
             {
-                platformPermutationToSpawn = Random.Range(0, platformPermutationsList.Length);
+                //Get a platform from the platform pool
+                GameObject obj = ObjectPooler.current.GetPooledObject();
 
-                //If the permutation we got was not the same as the last permutation spawned, break out of while
-                if (platformPermutationToSpawn != lastPlatformPermutationSpawned)
-                {
-                    lastPlatformPermutationSpawned = platformPermutationToSpawn;
-                    differentPermutation = true;
-                }
+                //Setting up platform location and size
+                //Random.Range(-0.5f, 0.5f) offset to make platforms not spawn right on top of each other (hopefully)
+                //Change size based on the three platform sizes initiated, can tune this if needed
+                obj.transform.position = new Vector3(transform.position.x + Random.Range(-5.3f, 5.3f), 0f + (Random.Range(5, 6f) * (i + 1)), transform.position.z);
+                obj.transform.rotation = transform.rotation;
+                obj.transform.localScale = new Vector3(platformSizes[Random.Range(0, platformSizes.Length)], obj.transform.localScale.y, obj.transform.localScale.z);
+
+                //Enable the object
+                obj.SetActive(true);
             }
-
-            GameObject platformPermutationObj = (GameObject)Instantiate(platformPermutationsList[platformPermutationToSpawn]);
-            platformPermutationObj.transform.position = new Vector3(transform.position.x, transform.position.y + (6f * i), transform.position.z);
-
-            platformPermutationObj.SetActive(true);
         }
 
         //Kick off another spawn after spawnTime
-        Invoke("Spawn", spawnTime);
+        Invoke("Spawn", Random.Range(spawnMin, spawnMax));
     }
 }
