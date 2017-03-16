eaee65d23115a348d60311a6795f80e47e16e861
Grant Hirahara <ghirahar@uci.edu>
2017-03-15 20:23:57 -0700

    Added Gun

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -4,13 +4,58 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //public static GameObject character;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    PlayerController player;
+    //public float ammo;
+    private float currentRange;
+    public float bulletRange;
+    public float rangeDecrease;
+    public float rangeRecharge;
+    public string PLAYER_INPUT_FIRE;
+    public bool faceRight;
+
+    private void Awake()
+    {
+        player = GetComponent<PlayerController>();
+    }
+    private void Start()
+    {
+        currentRange = bulletRange;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown(PLAYER_INPUT_FIRE))
+        {
+            if (currentRange < 0)
+                currentRange = 0;
+            if (currentRange > rangeDecrease)
+                Fire();
+        }
+        if (currentRange < bulletRange)
+            currentRange += rangeRecharge * Time.fixedDeltaTime;
+        faceRight = player.GetDirection();
+    }
+    void Fire()
+    {
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        if (faceRight)
+        {
+            bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.right * 25;
+
+        }
+        else if (!faceRight)
+        {
+            bullet.GetComponent<Rigidbody2D>().velocity = -bullet.transform.right * 25;
+        }
+
+        Destroy(bullet, currentRange);
+        currentRange -= rangeDecrease;
+    }
 }
